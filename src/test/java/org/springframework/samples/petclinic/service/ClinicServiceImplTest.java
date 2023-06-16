c23c12494251d2cc46770bd1328046d76c789baf
Pavel Glukhov <pvglukhov@gmail.com>
2023-06-16 15:13:10 +0300

    [master] initial commit

--- src/test/java/org/springframework/samples/petclinic/service/ClinicServiceImplTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/ClinicServiceImplTest.java
@@ -15,9 +15,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
