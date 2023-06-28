c60165b5b99f5c0f6df4b8a5a0ef3e9696be58e9
Pavel Glukhov <pgluhov@verci.ru>
2023-06-28 17:33:08 +0300

    [master] initial commit

--- src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterYannyTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterYannyTest.java
@@ -9,16 +9,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
+
 @ActiveProfiles("base-test")
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {BaseConfig.class, LaurelConfig.class})
-public class HearingInterpreterTest {
+@ContextConfiguration(classes = {BaseConfig.class, YannyConfig.class})
+public class HearingInterpreterYannyTest {
     @Autowired
     HearingInterpreter hearingInterpreter;
 
     @Test
     public void whatIHeard() {
         String word = hearingInterpreter.whatIHeard();
-        assertEquals("Laurel", word);
+        assertEquals("Yanny", word);
     }
 }
